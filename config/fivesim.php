433aa8f8adc565dccad3466daaa203393fe32bf6
Marcos Seko <marcosseko.travail@gmail.com>
2026-08-20 06:42:07 +0100

    Add config/fivesim.php reading FIVESIM_* environment variables

---
+++ config/fivesim.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'api_key' => env('FIVESIM_API_KEY'),
+
+    'base_url' => env('FIVESIM_BASE_URL', 'https://5sim.net/v1'),
+
+    'margin_percent' => env('FIVESIM_MARGIN_PERCENT', 20),
+
+];
